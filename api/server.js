2a610dcf24a3bbcc89944e983c4fb3417f3f3727
Dil Sombayeva <dilyasom@gmail.com>
2019-05-17 08:51:34 -0700

    server.js

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const db = require('../database/dbConfig');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors);
+
+server.get('/', (req, res) => {
+ res.send('Home page!');
+});
+
+server.use('/api/auth', authRouter);
+server.user('/api/users', usersRouter);
+
+module.exports = server;
