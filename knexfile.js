0b88929867e568beb5276ad8d59a4623ed364ae4
Dil Sombayeva <dilyasom@gmail.com>
2019-05-19 16:43:15 -0700

    white space

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,20 @@
-// Update with your config settings.
-
 module.exports = {
  development: {
   client: 'sqlite3',
+  useNullAsDefault: true,
   connection: {
    filename: './database/auth.db3',
   },
-  useNullAsDefault: true,
- },
- pool: {
-  afterCreate: (conn, done) => {
-   conn.run('PRAGMA foreign_keys = ON', done);
+  pool: {
+   afterCreate: (conn, done) => {
+    conn.run('PRAGMA foreign_keys = ON', done);
+   },
   },
- },
- migrations: {
-  directory: './database/migrations',
- },
- seeds: {
-  directory: './database/seeds',
+  migrations: {
+   directory: './database/migrations',
+  },
+  seeds: {
+   directory: './database/seeds',
+  },
  },
 };
