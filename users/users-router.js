14f476da0418c9a6923f48a91c3cc651a12b7fb4
Dil Sombayeva <dilyasom@gmail.com>
2019-05-19 16:45:22 -0700

    add users-router

--- users/users-router.js
+++ users/users-router.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
-const Users = require('./users-model');
 
-router.get('/', (req, res) => {});
+const Users = require('./users-model.js');
+// const restricted = require('../auth/restricted-middleware.js');
+
+router.get('/', (req, res) => {
+ Users.find()
+  .then(users => {
+   res.json(users);
+  })
+  .catch(err => res.send(err));
+});
+
+// function withRole(role) {
+//   return function(req, res, nex) {
+//     if (req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+//       next();
+//     } else {
+//       res.status(403).json({ message: 'you sir have no power here' });
+//     }
+//   };
+// }
+
+module.exports = router;
